6ed7e55cf84b5609ff7ab5b563a5d1df57028f65
samankeng <102471000+samankeng@users.noreply.github.com>
2025-06-10 21:41:22 +0100

    Update base.py

--- server/site_analyser/settings/base.py
+++ server/site_analyser/settings/base.py
@@ -201,7 +201,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 1000,
     # Add this to disable browsable API
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
